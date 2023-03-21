a19f4ab85c89a43326680fd4404bcaa0c2377f4f
Xeno <xeno.song@outlook.com>
2023-03-21 12:14:16 +0900

    [ADD] Modify drawer style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:word_book/View/Main.dart';
+import 'package:word_book/View/main.dart';
 
 void main() {
   print("Test Hello World!");
@@ -71,21 +71,44 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       drawer: Drawer(
-          child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          ListTile(
+        backgroundColor: const Color.fromARGB(0xFF, 0x1C, 0x1B, 0x1F),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const UserAccountsDrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.purple,
+              ),
+              accountName: Text("Xeno.Song"),
+              accountEmail: Text("H202046033@hycu.ac.kr"),
+              currentAccountPicture: CircleAvatar(backgroundImage: AssetImage('assets/image/icon.jpg')),
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.apps,
+                color: Colors.white70,
+              ),
               title: const Text('Main'),
+              textColor: Colors.white70,
+              style: ListTileStyle.drawer,
               onTap: () {
                 print("Drawer - Main Tap");
-              }),
-          ListTile(
+              },
+            ),
+            ListTile(
+              leading: const Icon(
+                Icons.copy,
+                color: Colors.white70,
+              ),
               title: const Text('Flash Card'),
+              textColor: Colors.white70,
               onTap: () {
                 print("Drawer - Flash Card Tap");
-              })
-        ],
-      )),
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(widget.appbarHeight),
         child: AppBar(
